02547f2cb56df447425ac921cdb9392fdc70c70b
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-03-03 18:33:03 +0000

    created html files

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const amenityDict = {};
+
+	$('input[type="checkbox"]').change(function () {
+		const amenityID = $(this).attr('id');
+		const amenityName = $(this).attr('name');
+
+		if ($(this).prop('checked')) {
+			amenityDict[amenityID] = amenityName;
+		} else {
+			delete amenityDict[amenityID];
+						            }
+
+		updateAmenitiesList();
+	});
+
+	function updateAmenitiesList() {
+		const amenitiesList = Object.values(amenityDict).join(', ');
+		$('.popover h4').text(amenitiesList);
+	}
+	// get API status
+	$.get('http://0.0.0.0:5001/api/v1/places_search/', function(data) {
+		if (data.status === 'OK') {
+			$('#api_status').addClass('available');
+		} else {
+			$('#api_status').removeClass('available');
+		}
+	});
+});
