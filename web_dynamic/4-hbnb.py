07d5c9547358c4dffcaea419611aeeecb2ffbaee
DeFatso <115782937+DeFatso@users.noreply.github.com>
2024-03-03 23:32:04 +0200

    task 5

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/3-hbnb/', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -38,7 +38,7 @@ def hbnb():
     """ add a variable cache_id """
     cache_id = str(uuid.uuid4())
 
-    return render_template('3-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
@@ -47,5 +47,5 @@ def hbnb():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
 
